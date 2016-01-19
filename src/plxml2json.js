874ad401d2e022e4bbdd83af6e93675bcedce66f
Appan Ponnappan <appanp@tejassoftware.com>
2016-01-19 13:34:02 +0530

    Added xform pipe to file, but end evt not firing

--- src/plxml2json.js
+++ src/plxml2json.js
@@ -11,17 +11,27 @@ function PlineXformer(filename, options) {
 	//Transformer & a few state variables
 	this.xform = new stream.Transform(options);
 	this.segCntInBranch = 0;
+	this.segNesting = [];
+	this.lBranchName = '';
 	this.xform._transform = function (data, encoding, done) {
-		console.log("... transform called");
+		//console.log("... transform called");
+		console.log("...segNesting depth: " + self.segNesting.length);
+		var opObj = {
+			'path': self.lBranchName + "." + self.segNesting.join('.') + self.segCntInBranch
+		};
+		this.push(opObj);
 	};
 	this.ipStream = fs.createReadStream(filename);
 	this.xmlStream = new XmlStream(this.ipStream);
 	//Read the required nodes & input to transform stream
+	this.xmlStream.on('startElement: branch', function (el) {
+		self.lBranchName = el.$.basename;
+	});
 	this.xmlStream.on('startElement: segment', function (el) {
+		self.xform.write(JSON.stringify(el));
 		self.segCntInBranch++;
 	});
 	this.xmlStream.on('endElement: segment', function (el) {
-		self.xform.write(JSON.stringify(el));
 		self.segCntInBranch--;
 	});
 	this.xmlStream.on('end', function () {
@@ -34,6 +44,8 @@ module.exports = function (options) {
 
 /* START: Main, Test the transformer */
 //var ipFile = require('path').join(__dirname, process.argv[2]);
-var ipFile1 = 'test/pl_xml/unit/OnRequest_1sub_1start_1end.xml';
-//var ipFile = require('path').join(__dirname, ipFile1);
-var plXform = new PlineXformer(ipFile1);
+var ipFile = process.argv[2];
+//var ipFile = 'test/pl_xml/unit/OnRequest_1sub_1start_1end.xml';
+var plXform = new PlineXformer(ipFile);
+var opStream = fs.createWriteStream('./output.dot');
+plXform.xform.pipe(opStream);
